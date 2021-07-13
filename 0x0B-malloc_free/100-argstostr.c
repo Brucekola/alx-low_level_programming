f461a9d27e639acf78f12f0c5e8fd337f5e6465d
Brucekola <brucechilenga24@gmail.com>
2021-07-13 13:20:58 -0700

    Added adnced task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,44 +1,40 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
- * argstostr - A function that concatenates all arguments of your program
- * @ac: number of arguments
- * @av: array containing arguments
- * Return: A pointer to string that containing all arguments
- * or NULL if ac == 0, if av == NULL, or upon failure
- */
+* *argstostr - concatenates all arguements to the program
+* @ac: arguement count
+* @av: pointer to arguements
+* Return: pointer to new space in memory or null
+**/
 char *argstostr(int ac, char **av)
 {
-	char *new_str;
-	int len = 0, i = 0, j, k = 0;
+char *strDup;
+int i, j, k, size;
 
-	if (ac <= 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
+size = 0;
 
-	for (; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			len++;
-		len++;
-	}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+size++;
+size++;
+}
+size++;
 
-	len++;
-	new_str = malloc(len * sizeof(char));
-	if (new_str == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			new_str[k] = av[i][j];
-			k++;
-		}
-		new_str[k] = '\n';
-		k++;
-	}
-
-	new_str[k] = '\0';
-	return (new_str);
+strDup = malloc(sizeof(char) * size);
+if (strDup == NULL)
+return (NULL);
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+strDup[k++] = av[i][j];
+}
+strDup[k++] = '\n';
+}
+strDup[k] = '\0';
+return (strDup);
 }
