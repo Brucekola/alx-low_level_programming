68d9ab5811f66eb188fb67ef6b71f079e703c262
Brucekola <brucechilenga24@gmail.com>
2021-08-09 10:12:23 -0700

    redid task 3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,16 @@
-#include "main.h"
-#include <stdio.h>
 #include "holberton.h"
 
 /**
-  * append_text_to_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file.
+ * @text_content: text to be appended at EOF.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wr, len;
 
 	if (!filename)
 		return (-1);
@@ -19,29 +18,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+		return (1);
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
 
-	if (text_content)
-	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
-	}
+	wr = write(fd, text_content, len);
+	if (wr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
